d5edfc58274b2e1a2d68f93a90a5ecc9ed2df58a
Crypto Rookies <patrick@halcyondao.finance>
2023-11-20 14:47:08 +0800

    initial code for bounding curve

--- smooth_bot/lib/src/main.rs
+++ smooth_bot/lib/src/main.rs
@@ -1,3 +1,33 @@
-pub fn test() {
-    println!("testing the library!");
+pub struct bd {
+    pub X: f64,
+    pub Y: f64,
+    pub K: i128,
+}
+
+impl bd {
+    pub fn new(X: f64, Y: f64, K: i128) -> Self {
+        Self {
+            X,
+            Y,
+            K,
+        }
+    }
+    pub fn price_of_Y(self) -> f64 { // what is the price of Y in comparison to X
+        let price = (self.X / self.Y) as f64;
+        return price;
+    }
+    pub fn price_of_X(self) -> f64 { // what is the price of X in comparison to Y
+        let price = (self.Y / self.X) as f64;
+        return price;
+    }
+    pub fn swap_X_Y(&mut self, x: f64) -> f64 { // how many X will be swapped to Y, return quantity of Y that was swapped
+        self.X += x;
+        self.Y = (self.K / self.X as i128) as f64;
+        return self.Y
+    }
+    pub fn swap_Y_X(&mut self, y: f64) -> f64 { // how many Y will be swapped to X, return quantity of X that was swapped
+        self.Y +=  y;
+        self.X = (self.K / self.Y as i128) as f64;
+        return self.X
+    }
 }
--- smooth_bot/src/main.rs
+++ smooth_bot/src/main.rs
@@ -1,27 +1,43 @@
-use plotly::common::Title;
-use plotly::layout::{Axis, Layout, Legend};
-use plotly::{Plot, Scatter};
+//use plotly::common::Title;
+//use plotly::layout::{Axis, Layout, Legend};
+//use plotly::{Plot, Scatter};
+
 pub mod random_swap;
-use boundingcurve::test;
+use boundingcurve::{*};
+use std::{thread, time};
 
 fn main() {
 
     println!("testing random number is {}", random_swap::swap());
-    boundingcurve::test();
+    let boundingcurve = boundingcurve::bd::new(1000.0, 500.0, (1000.0 * 500.0) as i128); // initialize a new bounding curve with X = 1000.0, Y = 500.0
 
-    let layout = Layout::new()
-        .title(Title::new("Price Chart Protected with MMaaS"))
-        .x_axis(Axis::new().title(Title::new("Time")))
-        .y_axis(Axis::new().title(Title::new("Price")))
-        .legend(Legend::new());
+    println!("X = {} and Y = {}", boundingcurve.X, boundingcurve.Y);
 
-    let x_values= vec![1, 2, 3, 4, 5];
-    let y_values = vec![2, 4, 6, 8, 10];
+    //let layout = Layout::new()
+    //    .title(Title::new("Price Chart Protected with MMaaS"))
+    //    .x_axis(Axis::new().title(Title::new("Time")))
+    //    .y_axis(Axis::new().title(Title::new("Price")))
+    //    .legend(Legend::new());
 
-    let scatter = Scatter::new(x_values, y_values).name("Token X - Price Chart");
+    //let x_values= vec![1, 2, 3, 4, 5];
+    //let y_values = vec![2, 4, 6, 8, 10];
 
-    let mut plot = Plot::new();
-    plot.add_trace(scatter);
-    plot.set_layout(layout);
-    plot.show();
+    //let scatter = Scatter::new(x_values, y_values).name("Token X - Price Chart");
+
+    //let mut plot = Plot::new();
+    //plot.add_trace(scatter);
+    //plot.set_layout(layout);
+    //plot.show();
+
+    thread::sleep(time::Duration::from_millis(100));
+
+
+
+    //let x_values2= vec![5, 4, 3, 4, 5];
+    //let y_values2 = vec![3, 4, 6, 8, 10];
+    //let scatter2 = Scatter::new(x_values2, y_values2).name("Token X - Price Chart");
+
+    //plot.add_trace(scatter2);
+    //plot.show();
+
 }
